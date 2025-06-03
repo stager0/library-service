94f78aead11357c397bacacf49327beaf1872db5
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-03 20:15:27 +0200

    feat: implement Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=50, choices=Cover)
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=8)

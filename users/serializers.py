440782a367508ad3cbd1165f42c3aaa18c016cff
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-03 22:42:24 +0200

    refactor: correct UserSerializer name

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from users.models import User
 
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "email", "password", "is_staff")

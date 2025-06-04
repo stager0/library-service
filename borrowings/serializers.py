f36fe9455988b8f2e80e1721c0025771e69e310f
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-04 20:20:47 +0200

    feat: implement BorrowingCreateSerializer and also atomic create method inside.

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,5 +1,7 @@
+from django.db import transaction
 from rest_framework import serializers
 
+from books.models import Book
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
 
@@ -12,3 +14,24 @@ class BorrowingSerializer(serializers.ModelSerializer):
 
 class BorrowingReadSerializer(BorrowingSerializer):
     book = BookSerializer(many=True, read_only=True)
+
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book")
+        read_only_fields = ("id",)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        user = self.context["request"].user
+        borrowing = Borrowing.objects.create(user=user, **validated_data)
+        book = borrowing.book
+        if book.inventory == 0:
+            raise ValueError("There is no this book more")
+
+        book.inventory -= 1
+        book.save()
+        borrowing.save()
+
+        return borrowing

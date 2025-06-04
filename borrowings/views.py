6403af03f5b9a1beed73845b95219e1f91d45596
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-04 18:53:29 +0200

    feat: add method get to BorrowingView

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,21 +1,23 @@
 from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from books.permissions import IsAdminOrAllowAnyReadOnly
+from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingReadSerializer
 
 
-# Create your views here.
 class BorrowingView(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     GenericAPIView
-    ):
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (IsAdminOrAllowAnyReadOnly,)
+):
+    queryset = Borrowing.objects.all()
 
     def get_serializer_class(self):
         if self.request.method == "GET" and "pk" in self.kwargs:
             return BorrowingReadSerializer
         return BorrowingSerializer
+
+    def get(self, request, *args, **kwargs):
+        if "pk" in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)

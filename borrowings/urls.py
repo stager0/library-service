db1736b64b8cde710e90a1df255b68b3d67893a3
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-04 22:03:05 +0200

    feat: register return-book path

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from borrowings.views import BorrowingView
+from borrowings.views import BorrowingView, BorrowingReturnView
 
 
 app_name = "borrowings"
 urlpatterns = [
     path("", BorrowingView.as_view(), name="borrowings-list"),
-    path("<int:pk>/", BorrowingView.as_view(), name="borrowing-detail")
+    path("<int:pk>/", BorrowingView.as_view(), name="borrowing-detail"),
+    path("<int:pk>/return-book/", BorrowingReturnView.as_view(), name="return-book")
 ]

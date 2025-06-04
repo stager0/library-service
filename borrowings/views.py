77e0cd856d52f7caf1e2f48ececbe239e0e2d26b
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-04 17:17:58 +0200

    feat: implement BorrowingView

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from books.permissions import IsAdminOrAllowAnyReadOnly
+from borrowings.serializers import BorrowingSerializer, BorrowingReadSerializer
+
 
 # Create your views here.
+class BorrowingView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericAPIView
+    ):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAdminOrAllowAnyReadOnly,)
+
+    def get_serializer_class(self):
+        if self.request.method == "GET" and "pk" in self.kwargs:
+            return BorrowingReadSerializer
+        return BorrowingSerializer

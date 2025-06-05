224d3d730bbc5a268adbd51b75f9707e9820e429
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-05 15:36:53 +0200

    feat: implement UserProfile model

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    email = models.EmailField(unique=True)
+    telegram_chat_id = models.IntegerField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.email

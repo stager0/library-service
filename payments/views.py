9445eb9f164af83978f5a3ad2bacfd9395381e5a
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-06 23:46:25 +0200

    feat: implement PaymentView (returns only user's payments)

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowings.models import Borrowing
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericAPIView
+):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated,]
+
+    def get_queryset(self):
+        queryset = Payment.objects.all()
+        if not self.request.user.is_staff:
+            borrowings_ids = Borrowing.objects.filter(user=self.request.user.id).values_list("id", flat=True)
+            return queryset.filter(borrowing_id__in=borrowings_ids)
+
+        if self.request.user.is_staff:
+            return queryset

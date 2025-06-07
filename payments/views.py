30776a1bc4b03c3fbb8f5615af15d53d4795fc39
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 15:27:33 +0200

    feat: implement CreateCheckoutSessionView

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,10 @@
-from rest_framework import mixins
+import stripe
+from rest_framework import mixins, status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
 
 from borrowings.models import Borrowing
 from payments.models import Payment
@@ -23,3 +27,30 @@ class PaymentView(
 
         if self.request.user.is_staff:
             return queryset
+
+
+class CreateCheckoutSessionView(APIView):
+    def post(self, request):
+        serializer = PaymentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            money_to_pay = serializer.validated_data["money_to_pay"]
+            borrowing_id = serializer.validated_data["borrowing_id"]
+            borrowing_obj = Borrowing.objects.get(id=borrowing_id)
+
+            session = stripe.checkout.Session.create(
+                payment_method_types=["card"],
+                line_items=[{
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {
+                            "name": borrowing_obj.book.title
+                        },
+                        "unit_amount": money_to_pay,
+                    },
+                    "quantity": 1
+                }],
+                mode="payment"
+            )
+            return Response({"checkout_url": session.url}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

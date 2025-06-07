ca7f06ea55b185ba5b869ecabb797196c962cd91
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 22:36:58 +0200

    feat: implement success-pay/ and cancel-pay/ urls (payments)

--- payments/urls.py
+++ payments/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from payments.views import CreateCheckoutSessionView, PaymentView
-
+from payments.views import CreateCheckoutSessionView, PaymentView, SuccessPayView, CancelPayView
 
 app_name = "payments"
 
 urlpatterns = [
     path("create-checkout-session/", CreateCheckoutSessionView.as_view(), name="create-checkout-session"),
     path("payment-list/", PaymentView.as_view(), name="payment_list"),
-    path("payment-list/<int:pk>/", PaymentView.as_view(), name="payment_detail")
+    path("payment-list/<int:pk>/", PaymentView.as_view(), name="payment_detail"),
+    path("success-pay/", SuccessPayView.as_view(), name="success-pay"),
+    path("cancel-pay/", CancelPayView.as_view(), name="cancel-pay")
 ]

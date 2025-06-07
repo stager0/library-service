cdd8ec19faed48284d517ed228b7eacc95e9be00
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 16:16:58 +0200

    feat: implement CreatePaymentSessionSerializer with validation for borrowing_id

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from borrowings.models import Borrowing
 from payments.models import Payment
 
 
@@ -8,3 +9,13 @@ class PaymentSerializer(serializers.ModelSerializer):
         model = Payment
         fields = ("id", "status", "type", "borrowing_id", "session_url", "session_id", "money_to_pay")
         read_only_fields = ("id",)
+
+
+class CreatePaymentSessionSerializer(serializers.Serializer):
+    money_to_pay = serializers.DecimalField(max_digits=8, decimal_places=2)
+    borrowing_id = serializers.IntegerField()
+
+    def validate_borrowing_id(self, value):
+        if not Borrowing.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Borrowing with given id not exists.")
+        return value

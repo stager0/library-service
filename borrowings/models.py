9f8caddedd1353be7614f47d6e48654c31c840bd
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 23:15:36 +0200

    feat: add pay_status field in Borrowing Model (also add PayStatusChoices with choices: PAID and PENDING)

--- borrowings/models.py
+++ borrowings/models.py
@@ -6,8 +6,19 @@ from users.models import User
 
 
 class Borrowing(models.Model):
+    class PayStatusChoices(models.TextChoices):
+        PAID = "Paid"
+        PENDING = "Pending"
+
     borrow_date = models.DateTimeField(default=timezone.now, blank=True)
     expected_return_date = models.DateTimeField(blank=True, null=True)
     actual_return_date = models.DateTimeField(blank=True, null=True)
+    pay_status = models.CharField(
+        null=True,
+        blank=True,
+        max_length=55,
+        choices=PayStatusChoices,
+        default="PENDING"
+    )
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)

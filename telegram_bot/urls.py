4a87d245402b8737bccaca9f2b62a3ef4ce998eb
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 17:14:09 +0200

    fix: add app_name in telegram_bot urls

--- telegram_bot/urls.py
+++ telegram_bot/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from .views import TelegramWebhookView
 
+
+app_name = "telegram_bot"
 urlpatterns = [
     path("webhook/", TelegramWebhookView.as_view(), name="telegram_webhook"),
 ]

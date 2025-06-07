052361379d0f6ec32a156f830d27f8094be97cb5
Andrii Shtaher <andriishtaher@gmail.com>
2025-06-07 23:31:11 +0200

    feat: implement create_fine_checkout_session method (it takes borrowing_id and count_of_delay_days and returns the session, also creates Payment object)

--- payments/views.py
+++ payments/views.py
@@ -1,13 +1,13 @@
 import os
 
 import stripe
+from django.contrib.auth import get_user_model
 from dotenv import load_dotenv
 from rest_framework import mixins, status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import action
 
 from borrowings.models import Borrowing
 from payments.models import Payment
@@ -15,10 +15,12 @@ from payments.serializers import (
     PaymentSerializer,
     CreatePaymentSessionSerializer
 )
+from telegram_bot.models import UserProfile
+from telegram_bot.views import send_message
 
-
-SUCCESS_URL=f"https://{os.getenv("WEBHOOK_WITHOUT_PROTOCOL_AND_PATH")}/api/payments/payment-list/"
-CANCEL_URL=f"https://{os.getenv("WEBHOOK_WITHOUT_PROTOCOL_AND_PATH")}/api/payments/create-checkout-session/"
+FINE_MULTIPLIER = 2
+SUCCESS_URL=f"https://{os.getenv('WEBHOOK_WITHOUT_PROTOCOL_AND_PATH')}/api/payments/success-pay/?session_id="
+CANCEL_URL=f"https://{os.getenv('WEBHOOK_WITHOUT_PROTOCOL_AND_PATH')}/api/payments/cancel-pay/"
 
 load_dotenv()
 stripe.api_key = os.getenv("STRIPE_PRIVATE_KEY")
@@ -64,7 +66,7 @@ def create_checkout_session(borrowing_id: int):
             "quantity": 1
         }],
         mode="payment",
-        success_url=SUCCESS_URL,
+        success_url=SUCCESS_URL + "{CHECKOUT_SESSION_ID}",
         cancel_url=CANCEL_URL
     )
     Payment.objects.create(
@@ -73,6 +75,37 @@ def create_checkout_session(borrowing_id: int):
         session_url=session.url,
         session_id=session.id,
         money_to_pay=money_to_pay
+    )
+    return session
+
+def create_fine_checkout_session(borrowing_id: int, count_of_delay_days: int):
+    borrowing_obj = Borrowing.objects.get(id=borrowing_id)
+    overdue_fine = (count_of_delay_days * borrowing_obj.book.daily_fee) * FINE_MULTIPLIER
+    overdue_fine_in_cents = int(overdue_fine * 100)
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "product_data": {
+                    "name": f"Fine of overdue days for book: '{borrowing_obj.book.title}'\n "
+                            f"Count of overdue days: {count_of_delay_days}"
+                },
+                "unit_amount": overdue_fine_in_cents
+            },
+            "quantity": 1
+        }],
+        mode="payment",
+        success_url=SUCCESS_URL + "{CHECKOUT_SESSION_ID}",
+        cancel_url=CANCEL_URL
+    )
+    Payment.objects.create(
+        type="FINE",
+        borrowing_id=borrowing_id,
+        session_url=session.url,
+        session_id=session.id,
+        money_to_pay=overdue_fine
     )
     return session
 
